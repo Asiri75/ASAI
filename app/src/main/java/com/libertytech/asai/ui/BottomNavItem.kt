4428b8be62a759d49aff10eb73c154aabf7aad8e
Lukas Descoins <lukas@bailfacile.fr>
2023-06-06 15:38:05 +0200

    Add LukasScreen

--- app/src/main/java/com/libertytech/asai/ui/BottomNavItem.kt
+++ app/src/main/java/com/libertytech/asai/ui/BottomNavItem.kt
@@ -4,4 +4,5 @@ import com.libertytech.asai.R
 
 sealed class BottomNavItem(var title:String, var icon:Int, var screen_route:String){
     object Home : BottomNavItem("Home", R.drawable.ic_home,"home")
+    object Lukas : BottomNavItem("Lukas", R.drawable.ic_lukas, "lukas")
 }
