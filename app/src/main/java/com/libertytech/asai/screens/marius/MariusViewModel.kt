1a40c0809d10b642573d53caf20620fb3703982b
Anas Asiri <asiri-anas@live.fr>
2023-06-07 10:05:10 +0200

    Add usecase and example of coroutine

--- app/src/main/java/com/libertytech/asai/screens/marius/MariusViewModel.kt
+++ app/src/main/java/com/libertytech/asai/screens/marius/MariusViewModel.kt
@@ -1,7 +1,11 @@
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 data class MariusUiState(
         val currentScrambledWord: String = ""
@@ -18,6 +22,17 @@ class MariusViewModel: ViewModel() {
 
     fun handleClick() {
         _uiState.value = MariusUiState(currentScrambledWord = "Tu m'as touché !")
+    }
+
+
+    fun makeRequest(companyDescription: String){
+        CoroutineScope(Dispatchers.IO).launch {
+            //Appel bloquant
+
+            withContext(Dispatchers.Main) {
+                //Modifier uiState
+            }
+        }
     }
 
     init {
---
+++ app/src/main/java/com/libertytech/asai/usecases/SearchCompanyNamesUseCase.kt
@@ -0,0 +1,14 @@
+package com.libertytech.asai.usecases
+
+import com.aallam.openai.api.completion.TextCompletion
+import com.libertytech.asai.repositories.OpenAiRepository
+
+
+class TotoUsecase {
+
+    companion object {
+        const val COMPANY_NAME_REQUEST = ""
+    }
+
+
+}
